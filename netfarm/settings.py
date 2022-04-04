5608c0ee2fbc9fbac909538a8edba93bff7c1409
KNgenoBrian <KBrianNgeno@gmail.com>
2022-04-04 14:22:54 +0300

    prep for heroku deployment

--- netfarm/settings.py
+++ netfarm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dz)1%3(=j(rkty)x#bm-7!r*^5&x(pu$@bq99u#(lb7uxtuolm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['netfarm.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
