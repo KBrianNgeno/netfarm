1860267617d2001557711d76d1f370c69b931698
KNgenoBrian <KBrianNgeno@gmail.com>
2022-04-04 15:22:05 +0300

    reorder middleware

--- netfarm/settings.py
+++ netfarm/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-             BASE_DIR / 'templates'
+            # os.path.join(BASE_DIR, 'templates')
+            BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
